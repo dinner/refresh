cf3a1409e3773db19239e014f380d1dff88809e7
apple <apple@appledeMac-mini.local>
2019-12-27 17:18:24 +0800

    初步实现功能

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,16 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
+enum RefreshStatus{
+  DropSmallerCritical,DropOffControll,DropBiggerThanCritical
+}
+
+class _MyHomePageState extends State<MyHomePage> with SingleTickerProviderStateMixin{
   int _counter = 5;
   ScrollController _controller = new ScrollController();
-  double _criticalPos = -60;
-  String _prompt = '下拉刷新';
+  double _criticalPos = -80;
+  AnimationController ac;
+  RefreshStatus refStatus = RefreshStatus.DropBiggerThanCritical;
 
   void _incrementCounter() {
     setState(() {
@@ -39,16 +44,6 @@ class _MyHomePageState extends State<MyHomePage> {
     super.initState();
     _controller.addListener(() { 
       print(_controller.offset);
-      var offset = _controller.offset;
-      if(offset < _criticalPos){
-        _prompt = '松开刷新';
-      }
-      else{
-        _prompt = '下拉刷新';
-      }
-      setState(() {
-        
-      });
     });
   }
 
@@ -61,34 +56,116 @@ class _MyHomePageState extends State<MyHomePage> {
   void onPointUp(PointerUpEvent event){
     var offset = _controller.offset;
     if(offset < _criticalPos){//启动动画
-      _controller
-      .animateTo(offset+10,duration: Duration(milliseconds: 2000),curve: Curves.linear)
-      .whenComplete((){
-         _controller.animateTo(
-          -10,
-          duration: Duration(milliseconds: 100),
-          curve: Curves.linear);
+      setState(() {
+        refStatus = RefreshStatus.DropOffControll;
       });
+
+      _controller
+      .animateTo(offset+1,duration: Duration(milliseconds: 2000),curve: Curves.linear)
+      .whenComplete((){
+        _controller
+          .animateTo(0,duration: Duration(milliseconds: 100),curve: Curves.linear)
+          .whenComplete((){
+            setState(() {
+              refStatus = RefreshStatus.DropBiggerThanCritical;
+            });
+          });
+      });
+    }
+    else{
+      _controller
+          .animateTo(0,duration: Duration(milliseconds: 100),curve: Curves.linear)
+          .whenComplete((){
+            setState(() {
+              refStatus = RefreshStatus.DropBiggerThanCritical;
+            });
+          });
     }
   }
 
   void onPointMove(PointerMoveEvent event){
-    
+    var offset = _controller.offset;
+    if(offset < _criticalPos){
+      refStatus = RefreshStatus.DropSmallerCritical;
+    }
+    else{
+      refStatus = RefreshStatus.DropBiggerThanCritical;
+    }
+    setState(() {
+      
+    });
   }
 
+  Widget widgetHeader(RefreshStatus status){
+    Widget wid;
+    switch(status){
+      case RefreshStatus.DropBiggerThanCritical:{
+        wid = Container(
+          color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('asserts/dropDown.png',width: 24,height: 24),
+              Text('下拉刷新')
+            ],
+          ),
+        );
+      }break;
+      case RefreshStatus.DropOffControll:{
+        wid = Container(
+          color: Colors.white,
+          child:  Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Padding(
+                padding: EdgeInsets.only(right: 10),
+                child:
+                Container(
+                  width: 24,
+                  height: 24,
+                  child:
+              CircularProgressIndicator(
+                valueColor:new AlwaysStoppedAnimation<Color>(Colors.blue),
+                strokeWidth:3,
+              ))),
+              Text('正在刷新，请稍候')
+            ],
+          ),
+        );
+      }break;
+      case RefreshStatus.DropSmallerCritical:{
+          wid = Container(
+            color: Colors.white,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              Image.asset('asserts/dropUp.png',width: 24,height: 24),
+              Text('松开刷新')
+            ],
+          ),
+        );
+      }break;
+    }
+    return wid;
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(''),
+        title: Text('刷新'),
+        elevation: 0.0,
       ),
       body:Stack(
         alignment: AlignmentDirectional.center,
         children:<Widget>[
-          Positioned(top:-_controller.offset/2,
+          Positioned(top:-_controller.offset/2-25,
           child:
-          Text(_prompt,style: TextStyle(color: Colors.black),textAlign: TextAlign.center,),),
+          widgetHeader(refStatus)),
+          // Text(_prompt,style: TextStyle(color: Colors.black),textAlign: TextAlign.center,),),
       Listener(
         onPointerUp: onPointUp,
         onPointerMove: onPointMove,
